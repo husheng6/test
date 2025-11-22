883140ab17962e32a1aa6d2ff036d93bc81c14d2
husheng6 <2540809649@qq.com>
2025-11-22 14:13:57 +0800

    feature-multiply：新增乘法函数multiply

--- func.c
+++ func.c
@@ -1,4 +1,7 @@
-#include "func.h"
+
 int add(int a, int b) {
     return a + b;  // 基础加法实现
+}
+int multiply(int a, int b) {
+    return a * b;
 }
--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main() {
     int x = 10, y = 20;
     printf("Sum: %d\n", add(x, y));
+    printf("Multiply: %d\n", multiply(x, y));
     return 0;
 }
