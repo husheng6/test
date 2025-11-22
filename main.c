264ff60f59cb9ffb005a6244e6b9b08afaf25ff9
husheng6 <2540809649@qq.com>
2025-11-22 16:15:35 +0800

    Add comment to multiplication print statement

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main() {
     int x = 10, y = 20;
     printf("Sum: %d\n", add(x, y));
-    printf("Multiply: %d\n", multiply(x, y));
+    printf("Multiply: %d\n", multiply(x, y));//成员一注释
+
     return 0;
 }
